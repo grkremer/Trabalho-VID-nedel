241ec0cbf631fd64960a4b251eb4270d5f650f56
marcoantonioaav <56897818+marcoantonioaav@users.noreply.github.com>
2023-03-02 04:06:20 -0300

    Fornecedor

---
+++ Despesas cartao presidencial/Assets/Scripts/Fornecedor.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fornecedor
+{
+    private string cnpj;
+    private string nome;
+    private string atividadePrincipal;
+    private bool isMatriz;
+    private string naturezaJuridica;
+    private string municipio;
+    private string uf;
+    private string endereco;
+    private float latitude;
+    private float longitude;
+    private List<Despesa> despesas;
+
+    Fornecedor() {
+
+    }
+    public struct Despesa
+    {
+        float valor;
+        string tipo;
+        DateTime data;
+    }
+}
+
+
