2278b48cd3f493633b3caca283bdd165a85f0f05
marcoantonioaav <56897818+marcoantonioaav@users.noreply.github.com>
2023-03-16 11:54:58 -0300

    Fornecedores no mapa

--- Despesas cartao presidencial/Assets/Scripts/Fornecedor.cs
+++ Despesas cartao presidencial/Assets/Scripts/Fornecedor.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Fornecedor
 {
@@ -12,18 +13,48 @@ public class Fornecedor
     private string municipio;
     private string uf;
     private string endereco;
-    private float latitude;
-    private float longitude;
-    private List<Despesa> despesas;
+    public float latitude;
+    public float longitude;
+    private List<Despesa> despesas = new List<Despesa>();
 
-    Fornecedor() {
-
+    public Fornecedor(
+        string cnpj, 
+        string nome, 
+        string atividadePrincipal, 
+        bool isMatriz, 
+        string naturezaJuridica, 
+        string municipio, 
+        string uf, 
+        string endereco, 
+        float latitude, 
+        float longitude) {
+        this.cnpj = cnpj;
+        this.nome = nome;
+        this.atividadePrincipal = atividadePrincipal;
+        this.isMatriz = isMatriz;
+        this.naturezaJuridica = naturezaJuridica;
+        this.municipio = municipio;
+        this.uf = uf;
+        this.endereco = endereco;
+        this.latitude = latitude;
+        this.longitude = longitude;
     }
+
+    public void addDespesa(Despesa despesa) {
+        despesas.Add(despesa);
+    }
+
     public struct Despesa
     {
-        float valor;
-        string tipo;
-        //DateTime data;
+        public float valor;
+        public string tipo;
+        public DateTime data;
+
+        public Despesa(float valor, string tipo, DateTime data) {
+            this.valor = valor;
+            this.tipo = tipo;
+            this.data = data;
+        }
     }
 }
 
--- Despesas cartao presidencial/Assets/Scripts/MapPinner.cs
+++ Despesas cartao presidencial/Assets/Scripts/MapPinner.cs
@@ -1,8 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 using Microsoft.Geospatial;
 using Microsoft.Maps.Unity;
+using System;
+using System.Globalization;
 
 public class MapPinner : MonoBehaviour
 {
@@ -13,18 +16,76 @@ public class MapPinner : MonoBehaviour
 
     //public LatLon location;
 
+    private Dictionary<string, Fornecedor> dados;
+
     // Start is called before the first frame update
     void Start()
     {
-        var mapPin = Instantiate(pin);
-        mapPin.Location = new LatLon(0, 0);
-        pinLayer.MapPins.Add(mapPin);
         //MapPin.UpdateScales(new List<MapPin>(){mapPin}, mapRenderer);
+        dados = importaDados("Dados_processados_COMPLETO_CHEIO.csv");
+        foreach(Fornecedor fornecedor in dados.Values) {
+            var mapPin = Instantiate(pin);
+            mapPin.Location = new LatLon(fornecedor.latitude, fornecedor.longitude);
+            pinLayer.MapPins.Add(mapPin);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    Dictionary<string, Fornecedor> importaDados(string arquivo){
+        Dictionary<string, Fornecedor> dados = new Dictionary<string,Fornecedor>();
+        
+        StreamReader strReader = new StreamReader("Assets/" + arquivo);
+        string line = strReader.ReadLine();
+        Dictionary<string, int> col = leNomesColuna(line);
+        for(line = strReader.ReadLine(); line != null; line = strReader.ReadLine()) {
+            string[] itens = line.Split('$');
+            Debug.Log(line);
+
+            float valor = float.Parse(itens[col["VALOR"]]);
+            string tipo = itens[col["TIPO"]];
+            DateTime data = DateTime.Parse(itens[col["DATA PGTO"]]);
+            Fornecedor.Despesa despesa = new Fornecedor.Despesa(valor, tipo, data);
+
+            string cnpj = itens[col["CPF/CNPJ FORNECEDOR"]];
+            if(!dados.ContainsKey(cnpj)) {
+                string nome = itens[col["NOME FORNECEDOR"]];
+                string atividadePrincipal = itens[col["ATIVIDADE_PRINCIPAL"]];
+                bool isMatriz = "MATRIZ" == itens[col["MATRIZ"]];
+                string naturezaJuridica = itens[col["NATUREZA_JURIDICA"]];
+                string municipio = itens[col["MUNICIPIO"]];
+                string uf = itens[col["UF"]];
+                string endereco = itens[col["ENDEREÇO"]];
+                float latitude = float.Parse(itens[col["Latitude"]], CultureInfo.InvariantCulture.NumberFormat);
+                float longitude = float.Parse(itens[col["Longitude"]], CultureInfo.InvariantCulture.NumberFormat);
+                Fornecedor fornecedor = new Fornecedor(
+                    cnpj, 
+                    nome, 
+                    atividadePrincipal, 
+                    isMatriz, 
+                    naturezaJuridica, 
+                    municipio, 
+                    uf, 
+                    endereco, 
+                    latitude, 
+                    longitude
+                );
+                dados[cnpj] = fornecedor;
+            }
+            dados[cnpj].addDespesa(despesa);
+        }
+        return dados;
+    }
+    
+    Dictionary<string, int> leNomesColuna(string linha) {
+        Dictionary<string, int> coluna = new Dictionary<string, int>();
+        string[] itens = linha.Split('$');
+        for(int i = 0; i < itens.Length; i++)
+            coluna.Add(itens[i], i);
+        return coluna;
     }
 }
